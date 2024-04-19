7ff8618ad515e19ddf66a7bad2e1c3b51fc22da8
Bryce Knight <bkni0201@uni.sydney.edu.au>
2024-04-19 18:06:18 +1000

    Merged socket_routes into main file to avoid importing issues, and so errors in socket_routes show up in log

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ the socket event handlers are inside of socket_routes.py
 '''
 
 from flask import *
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, emit, leave_room
 import db
 from models import *
 import secrets
 import bcrypt
-# don't remove this, even though not accessed!!!
-import socket_routes
 import logging
+
+room = Room()
 
 # this turns off Flask Logging, uncomment this to turn off Logging
 # log = logging.getLogger('werkzeug')
@@ -168,6 +168,81 @@ def friends_request():
     return url_for("friends")
 
 
+
+# when the client connects to a socket
+# this event is emitted when the io() function is called in JS
+@socketio.on('connect')
+def connect():
+    username = session_user(session)
+    room_id = request.cookies.get("room_id")
+    if room_id is None or username is None:
+        return
+    # socket automatically leaves a room on client disconnect
+    # so on client connect, the room needs to be rejoined
+    join_room(int(room_id))
+    emit("incoming", (f"{username} has connected", "green"), to=int(room_id))
+
+# event when client disconnects
+# quite unreliable use sparingly
+@socketio.on('disconnect')
+def disconnect():
+    username = session_user(session)
+    room_id = request.cookies.get("room_id")
+    if room_id is None or username is None:
+        return
+    emit("incoming", (f"{username} has disconnected", "red"), to=int(room_id))
+
+# send message event handler
+@socketio.on("send")
+def send(message):
+    username = session_user(session)
+    room_id = room.get_room_id(username)
+    emit("incoming", (f"{username}: {message}"), to=room_id)
+    
+# join room event handler
+# sent when the user joins a room
+@socketio.on("join")
+def join(receiver_name):
+    sender_name = session_user(session)
+    receiver = db.get_user(receiver_name)
+    if receiver is None:
+        return "Unknown receiver!"
+    sender = db.get_user(sender_name)
+    if sender is None:
+        return "Unknown sender!"
+
+    room_id = room.get_room_id(receiver_name)
+
+    # if the user is already inside of a room 
+    if room_id is not None:
+        
+        room.join_room(sender_name, room_id)
+        join_room(room_id)
+        # emit to everyone in the room except the sender
+        emit("incoming", (f"{sender_name} has joined the room.", "green"), to=room_id, include_self=False)
+        # emit only to the sender
+        emit("incoming", (f"{sender_name} has joined the room. Now talking to {receiver_name}.", "green"))
+        return room_id
+
+    # if the user isn't inside of any room, 
+    # perhaps this user has recently left a room
+    # or is simply a new user looking to chat with someone
+    room_id = room.create_room(sender_name, receiver_name)
+    join_room(room_id)
+    emit("incoming", (f"{sender_name} has joined the room. Now talking to {receiver_name}.", "green"), to=room_id)
+    return room_id
+
+# leave room event handler
+@socketio.on("leave")
+def leave():
+    room_id = room.get_room_id(username)
+    username = session_user(session)
+    emit("incoming", (f"{username} has left the room.", "red"), to=room_id)
+    leave_room(room_id)
+    room.leave_room(username)
+
+
+
 def session_user(session):
     '''
     Returns the username of the currently logged in user, or None if authentication unsuccessful
@@ -188,6 +263,8 @@ def username_error(username):
         if not (i.isalpha() or i.isnumeric or i in valid_characters):
             return "Error: Username contains invalid characters"
     return ""
+
+
 
 if __name__ == '__main__':
     # socketio.run(app)
